bb50f07b57ed759c7be5b4eb976582f165b5905e
Permlap <permlapteam@gmail.com>
2022-07-10 14:05:32 +0700

    first production test

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,8 @@ import Navbar from '../components/Navbar'
 function MyApp({ Component, pageProps }) {
   return(
   <StyledEngineProvider injectFirst>
-    <Head>
-   
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>  
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@1000;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
-    </Head>
-    <div className="md:hidden flex items-center  absolute text-center z-10 w-screen h-full bg-white text-3xl ">ยังไม่รองรับการแสดงผลบนมือถือ โปรดเข้า Website อีกครัั้งในอุปกร์คอมพิวเตอร์</div>
+    
+    <div className="md:hidden flex items-center  text-center z-10 w-full fixed h-full bg-white text-3xl ">ยังไม่รองรับการแสดงผลบนมือถือ โปรดเข้า Website อีกครัั้งในอุปกรณ์คอมพิวเตอร์หรือ tablet</div>
     <Navbar/>
    
     <Component {...pageProps} />
